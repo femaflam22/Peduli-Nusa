4f18f01830609b125929651d7466d404a1ecfae0
femaflam22 <femaflam22@gmail.com>
2021-04-19 11:05:06 +0700

    tailwind & responsif

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            lightgray: "#AAA69D",
+            titlegray: "#636774",
+            darkpink: "#C7458F",
+            lightblue: "#76DBE6",
+            cardwhite: "#F2F3F5",
+            cardgray: "#32282C",
+            textblack: "#33292C",
+            transparent: "transparent",
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
